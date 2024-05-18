6510a764309a0eebdb009aad1538b4ea9c71b76f
pilgrim_tabby <pilgrimtabby@gmail.com>
2024-05-18 23:29:23 +0900

    Configure build settings for MacOS
    Added build.sh, which compiles a MacOS executible using Pyinstaller
    and injects it into a Platypus application, along with a bootstrap
    script that starts it. Apparently it's infamously difficult to get a
    GUI app in Python to run on MacOS, so this is as good as it's going
    to get, I think. It's compiling fine right now (after lots of annoyances).

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+open "$SCRIPT_DIR"/"Pilgrim Autosplitter"
---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# To build on MacOS, navigate to the project root directory and run this (`bash scripts/build.sh`)
+pyinstaller src/pilgrim_autosplitter.py --noconfirm --onefile --windowed --name "Pilgrim Autosplitter"
+/usr/local/bin/platypus --background --quit-after-execution --app-icon 'resources/icon-macos.icns'  --name 'Pilgrim Autosplitter'  --interface-type 'None'  --interpreter '/bin/bash'  --author 'pilgrim_tabby'   --bundle-identifier org.pilgrimtabby.PilgrimAutosplitter --bundled-file 'dist/Pilgrim Autosplitter'  'scripts/bootstrap.sh'
+
+rm -r build
+rm -r dist
+
+mkdir dist
+mv "scripts/Pilgrim Autosplitter.app" "dist/Pilgrim Autosplitter.app"
