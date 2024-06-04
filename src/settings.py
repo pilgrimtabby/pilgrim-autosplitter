b9bb86227d1171181d0605a87bc029dc0cdad93d
pilgrim_tabby <pilgrimtabby@gmail.com>
2024-06-04 20:45:32 +0900

    Update version to v1.0.4

--- src/settings.py
+++ src/settings.py
@@ -43,7 +43,7 @@ COMPARISON_FRAME_WIDTH = 320
 COMPARISON_FRAME_HEIGHT = 240
 
 # Pilgrim Autosplitter's current version number
-VERSION_NUMBER = "v1.0.3"
+VERSION_NUMBER = "v1.0.4"
 
 # The URL of Pilgrim Autosplitter's GitHub repo
 REPO_URL = "https://github.com/pilgrimtabby/pilgrim-autosplitter/"
