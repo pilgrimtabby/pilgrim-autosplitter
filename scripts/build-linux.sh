1b0d4e6da47f207c90fc35519eab61e81b9fddee
pilgrim_tabby <pilgrimtabby@gmail.com>
2024-07-17 22:58:29 -0600

    Update Linux build script

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# Run this simple script to build a PyInstaller executible on Linux
+# Run this script to build a PyInstaller executible on Linux
 
-pyinstaller  --windowed --onefile src/pilgrim_autosplitter.py
+path/to/pyinstaller --windowed --onefile src/pilgrim_autosplitter.py
